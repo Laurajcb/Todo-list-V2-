436436f47b057b2f6e8dacaa3b98118582b36139
Laurajcb <callejaslaura96@gmail.com>
2022-11-22 09:23:50 -0500

    Fix typo on TodoHeader

--- curso-react-patrones-render/src/App/index.js
+++ curso-react-patrones-render/src/App/index.js
@@ -69,15 +69,17 @@ function App() {
       //   />
       // )}
       >
-        {todo => (
-          <TodoItem
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}
-          />
-        )}
+        {
+          todo => (
+            <TodoItem
+              key={todo.text}
+              text={todo.text}
+              completed={todo.completed}
+              onComplete={() => completeTodo(todo.text)}
+              onDelete={() => deleteTodo(todo.text)}
+            />
+          )
+        }
       </TodoList>
 
 
--- curso-react-patrones-render/src/TodoHeader/index.js
+++ curso-react-patrones-render/src/TodoHeader/index.js
@@ -7,9 +7,9 @@ function TodoHeader({ children, loading }) {
   return (
     <header>
       {
-      React.Children
-      .toArray(children)
-      .map(child => React.cloneElement(children, { loading }))
+        React.Children
+          .toArray(children)
+          .map(child => React.cloneElement(child, { loading }))
       }
     </header>
   )
